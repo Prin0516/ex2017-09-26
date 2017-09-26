ce411293a37ba41c0443435eb79ad7f56a49ce97
Prin0516 <105021046@live.aisa.edu.tw>
2017-09-26 12:15:48 +0800

    ex

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,13 +1,19 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
+
 public class MainFrame  extends Frame {
     private Button btn=new Button("add");
+    private Button subbtn=new Button("sub");
+    private Button exitbtn=new Button("exit");
     private Label lab=new Label("----");
+    private int labX=50,labY=50;
     private int count=0;
+    private Timer t1;
 
     public MainFrame(){
         init();
@@ -21,17 +27,46 @@ public class MainFrame  extends Frame {
                System.exit(0);
             }
         });
-        btn.setBounds(100,100,30,30);
+        btn.setBounds(300,300,30,30);
         this.add(btn);
-        lab.setBounds(100,200,30,30);
+        lab.setBounds(labX,labY,30,30);
         this.add(lab);
         btn.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 MainFrame.this.setTitle(Integer.toString(++count));
                 lab.setText(Integer.toString(count));
+                t1.start();
             }
         });
+        exitbtn.setBounds(100,300,30,30);
+        exitbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        this.add(exitbtn);
+        subbtn.setBounds(200,300,30,30);
+        subbtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                lab.setText(Integer.toString(--count));
+            }
+        });
+        this.add(subbtn);
+
+        t1=new Timer(500, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                count++;
+                lab.setText(Integer.toString(count));
+                labX+=2;
+                lab.setLocation(labX,labY);
+
+            }
+        });
+
 
 
     }
